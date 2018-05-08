c43c7b94fdddba8ddaa42a681765cedcc37b23cd
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-08 02:51:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38,40,40,37,39,37,39,66,65];
 let index = 0;
-const body = document.getElementsByTagName('body');
-function init(e) {
+
+function init() {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
-  if(key === code[index]) {
-    index++;
-    if(index === code.length) {
-      alert("Huzzah!");
+  document.body.addEventListener('keydown',
+    function(e){
+    let key = parseInt(e.detail || e.which);
+    console.log(key);
+    if(key === code[index]) {
+      index++;
+      if(index === code.length) {
+        alert("Huzzah");
+        index = 0;
+      }
+    }
+    else {
       index = 0;
     }
-  }
-  else {
-    index = 0;
-  }
-}
-body.addEventListener('keydown', init); {
-  
+  });
 }
