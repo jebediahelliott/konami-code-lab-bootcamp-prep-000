005566811945c52232b4bc10ed2c4a483d081b46
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-08 02:28:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ const code = ["ArrowUp", "ArrowUp", "ArrowDown",
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown',
-    huzzah);
+    huzzah(e));
   let index = 0;
   function huzzah(e) {
     
