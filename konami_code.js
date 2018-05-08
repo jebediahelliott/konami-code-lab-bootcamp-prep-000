adeb7a95f96af66028571b5f8a0affd23e2b47ca
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-08 02:43:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,10 @@
 const code = [38, 38,40,40,37,39,37,39,66,65];
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown',
-    huzzah);
+    function(e){;
     let key = parseInt(e.detail || e.which);
     console.log(key);
     if(key === code[index]) {
@@ -13,5 +14,8 @@ function init() {
         index = 0;
       }
     }
-  }
+    else {
+      index = 0;
+    }
+  });
 }
